031a325a2ee035838e71287c9c297f814249b4a9
GustavoRRSilva <gstvrrds@gmail.com>
2025-08-18 15:56:11 -0300

    feat:adding new appointment

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -7,9 +7,28 @@ import { Appointment } from '../models/appointment';
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
-  appointment: Appointment = {
-    id:1,
-    title:'Take dog for a walk',
-    data:new Date('2023-07-30'),
+  static id_:number = 1
+  newAppointmentTitle:string = ''
+  newAppointmentDate:Date = new Date()
+
+  appointments: Appointment[] = []
+
+  createAppointment():void{
+    if(this.newAppointmentDate && this.newAppointmentDate){
+      let newAppointment:Appointment = {
+        id:Date.now(),
+        title:this.newAppointmentTitle,
+        data:this.newAppointmentDate
+      }
+      this.appointments.push(newAppointment)
+      this.newAppointmentDate = new Date()
+      this.newAppointmentTitle = ''
+      return alert('Appointment created')
+    }
+    return alert("Values shouldn't be null")
+  }
+
+  showAppointment():Appointment[]{
+    return this.appointments
   }
 }
